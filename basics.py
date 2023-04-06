6c9b827f2d741da1ffc0dbb7fc3fb6ac3f59677c
Leto-du-Plessis <letoduplessis@gmail.com>
2023-04-06 17:22:51 +0200

    stopgap upload, dispatchers not working, varying amplitude is next task.

--- basics.py
+++ basics.py
@@ -1,14 +1,30 @@
 #IMPORTS
-import musicpy as mp
-from mp import C as C
+import numpy as np
+from scipy.io import wavfile
+from multipledispatch import dispatch
 
-guitar = (C('CM7', 3, 1/4, 1/8)^2 |
-          C('G7sus', 2, 1/4, 1/8)^2 |
-          C('A7sus', 2, 1/4, 1/8)^2 |
-          C('Em7', 2, 1/4, 1/8)^2 | 
-          C('FM7', 2, 1/4, 1/8)^2 |
-          C('CM7', 3, 1/4, 1/8)@1 |
-          C('AbM7', 2, 1/4, 1/8)^2 |
-          C('G7sus', 2, 1/4, 1/8)^2) * 2
+@dispatch(object, object)
+def generate_sine_wave(frequency, duration, amplitude = 4096, sample_rate = 44100):
+    t = np.linspace(0, duration, int(sample_rate * duration)) # discretized time interval
+    wave = amplitude * np.sin(2 * np.pi * frequency * t)      # generates our sine wave over the time interval constructed
+    return wave
 
-mp.play(guitar, bpm=100, instrument=25)
+@dispatch(object, object, int)
+def generate_sine_wave(frequency, duration, amplitude, sample_rate = 44100):
+    t = np.linspace(0, duration, int(sample_rate * duration)) # discretized time interval
+    wave = amplitude * np.sin(2 * np.pi * frequency * t)      # generates our sine wave over the time interval constructed
+    return wave
+
+@dispatch(object, object, object)
+def generate_sine_wave(frequency, duration, amplitude, sample_rate = 44100):
+    print('stonks')
+
+def varying_amplitude(function, duration):
+    print('stonks')
+
+def write_to_wav(waves, file_name = "generic.wav", rate = 44100, wav_type = np.int16):
+    wavfile.write(file_name, rate, data = waves.astype(wav_type)) 
+
+wave = generate_sine_wave(440, 5, amplitude = 'five')
+write_to_wav(wave)
+#wavfile.write("test.wav", rate = 44100, data = sine_wave.astype(np.int16))
